829cf1e8a29ab3ce4f1b00ff368c33d2edaf3d09
Higor Shindy Kudo <higorshun@icloud.com>
2020-05-18 13:25:23 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Higor Shindy Kudo on 2020/05/18.
+//  Copyright © 2020 Higor Shindy Kudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
